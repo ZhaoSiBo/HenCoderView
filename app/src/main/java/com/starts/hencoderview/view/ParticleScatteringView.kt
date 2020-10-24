a42bae94466f03418eaf2a24aea3384766d0ff6b
18772943719 <ztzhaosibo@gmail.com>
2020-10-24 18:46:37 +0800

    1.粒子动画自定义View

--- app/src/main/java/com/starts/hencoderview/view/ParticleScatteringView.kt
+++ app/src/main/java/com/starts/hencoderview/view/ParticleScatteringView.kt
@@ -10,6 +10,7 @@ import android.view.animation.LinearInterpolator
 import com.starts.hencoderview.R
 import kotlin.math.*
 import kotlin.random.Random
+import kotlin.system.measureTimeMillis
 
 /**
 
@@ -24,6 +25,8 @@ class ParticleScatteringView(context: Context, attrs: AttributeSet?) : View(cont
     var viewHeight = 0
 
 
+    var imageAngle = 0f
+
     var centerX = 0f
     var centerY = 0f
 
@@ -31,9 +34,19 @@ class ParticleScatteringView(context: Context, attrs: AttributeSet?) : View(cont
     lateinit var imageShader: BitmapShader
 
     //粒子的数量
-    val particleCount = 300
+    val particleCount = 500
     val particlePaint = Paint(Paint.ANTI_ALIAS_FLAG)
 
+
+    val imageBitmap: Bitmap by lazy {
+        val option = BitmapFactory.Options()
+        option.outWidth = (viewWidth * 1f / 2).toInt()
+        option.outHeight = (viewHeight * 1f / 2).toInt()
+        BitmapFactory.decodeResource(context.resources, R.mipmap.ui_scale_img, option)
+    }
+
+
+    val bitmapCanvas = Canvas()
     //粒子集合
     val particleList = ArrayList<Particle>()
 
@@ -43,39 +56,40 @@ class ParticleScatteringView(context: Context, attrs: AttributeSet?) : View(cont
     private var pathMeasure = PathMeasure()
 
     init {
-
-        particleAnimator.duration = 2000
+        particleAnimator.duration = 4000
         particleAnimator.repeatCount = -1
         particleAnimator.interpolator = LinearInterpolator()
         particleAnimator.addUpdateListener {
             updateParticle(it.animatedValue as Float)
+            imageAngle = 360 * it.animatedValue as Float
             invalidate()//重绘界面
         }
     }
 
     private fun updateParticle(fl: Float) {
         particleList.forEach {
+            if (it.offset > it.maxOffset) {
+                it.offset = 0f
+                it.speed = (Random.nextInt(2, 4)).toFloat()
+            }
+            it.alpha = ((1f - it.offset / it.maxOffset) * 225f).toInt()
 
+            it.offset += it.speed
+//            if(it.offset >=  it.maxOffset){
+//                return
+//            }else{
+            it.x = centerX + cos(it.angle) * (310f + it.offset)
+
+            if (it.y > centerY) {
+                it.y = (sin(it.angle) * (310f + it.offset) + centerY)
+            } else {
+                it.y = (centerY - sin(it.angle) * (310f + it.offset))
+            }
+
+            it.offset += it.speed
 
         }
     }
-
-    private fun fixXY(it: Particle) {
-
-        if (it.x.absoluteValue >= (cos(it.angle)) * it.maxOffset) {
-            it.x = ((viewWidth * 1 / 2) + min(
-                viewHeight * 1f,
-                viewWidth * 1f
-            ) / 2.7 * cos(it.angle * Math.PI / 180)).toFloat() + (10 - Random.nextInt(20))
-        }
-        if (it.y.absoluteValue >= (sin(it.angle)) * it.maxOffset) {
-            it.y = ((viewHeight * 1f / 2) + min(viewHeight * 1f, viewWidth * 1f) / 2.7 * sin(
-                it.angle * Math.PI / 180
-            )).toFloat() + (10 - Random.nextInt(20))
-        }
-
-    }
-
 
     override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
         super.onSizeChanged(w, h, oldw, oldh)
@@ -86,62 +100,66 @@ class ParticleScatteringView(context: Context, attrs: AttributeSet?) : View(cont
         centerY = viewHeight * 1f / 2
 
         innerCirclePath.addCircle(
-            viewWidth * 1f / 2,
-            viewHeight * 1f / 2,
-            min(viewHeight * 1f, viewWidth * 1f) / 3, Path.Direction.CCW
+            centerX,
+            centerY,
+            310f, Path.Direction.CCW
         )
 
         pathMeasure.setPath(innerCirclePath, false)
+        val singleLength = pathMeasure.length / particleCount
 
-        val imageBitmap: Bitmap by lazy {
-            val option = BitmapFactory.Options()
-            option.outWidth = (viewWidth * 1f / 2).toInt()
-            option.outHeight = (viewHeight * 1f / 2).toInt()
-            BitmapFactory.decodeResource(context.resources, R.mipmap.ui_scale_img, option)
-        }
+
+
 
         imageShader = BitmapShader(imageBitmap, Shader.TileMode.CLAMP, Shader.TileMode.CLAMP)
         imagePaint.shader = imageShader
 
-        val singleAngle = 360 * 1f / particleCount
 
         particlePaint.color = Color.WHITE
         particlePaint.style = Paint.Style.FILL
         for (i in 0..particleCount) {
             val particle = Particle()
+            val localArray = FloatArray(2)
+            val tanArray = FloatArray(2)
 
-            particle.angle = i * singleAngle
+            if (pathMeasure.getPosTan(i * singleLength, localArray, tanArray)) {
+                particle.x = localArray[0] + (Random.nextInt(10) - 20)
+                particle.y = localArray[1] + (Random.nextInt(10) - 20)
+            }
 
-            particle.x = ((viewWidth * 1 / 2) + min(
-                viewHeight * 1f,
-                viewWidth * 1f
-            ) / 2.7 * cos(particle.angle * Math.PI / 180)).toFloat() + (10 - Random.nextInt(20))
-
-            particle.y = ((viewHeight * 1f / 2) + min(viewHeight * 1f, viewWidth * 1f) / 2.7 * sin(
-                particle.angle * Math.PI / 180
-            )).toFloat() + (10 - Random.nextInt(20))
-
-            particle.speed = (Random.nextInt(10) + 5).toFloat()
-
-            particle.offset = cos(particle.angle) * particle.x
-
+            particle.speed = (Random.nextInt(2, 4)).toFloat()
+            particle.angle = acos((localArray[0] - centerX) / 280f)
             particleList.add(particle)
         }
         particleAnimator.start()
 
     }
 
+    val bitmap:Bitmap by lazy {
+        Bitmap.createBitmap(viewWidth, viewHeight,
+            Bitmap.Config.ARGB_8888)
+    }
+
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
+        canvas.save()
+
+        canvas.rotate(imageAngle , centerX , centerY)
         canvas.drawCircle(
-            viewWidth * 1f / 2,
-            viewHeight * 1f / 2,
-            min(viewHeight * 1f, viewWidth * 1f) / 3,
+            centerX,
+            centerY,
+            300f,
             imagePaint
         )
-        particleList.forEach {
-            canvas.drawCircle(it.x, it.y, it.radius, particlePaint)
+        canvas.restore()
+        val time = measureTimeMillis {
+            particleList.forEach {
+                particlePaint.alpha = it.alpha
+                canvas.drawCircle(it.x, it.y, it.radius, particlePaint)
+            }
         }
+
+        Log.d("ParticleScatteringView", "time = $time")
 
     }
 
