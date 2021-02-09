7cf540f3a93b66282e0ef7431e5b847a59fbee26
Starts <>
2021-02-09 15:17:06 +0800

    1.成功解决。。。和icon冲突问题

--- app/src/main/java/com/starts/hencoderview/EllipsizedTextScene.kt
+++ app/src/main/java/com/starts/hencoderview/EllipsizedTextScene.kt
@@ -1,8 +1,12 @@
 package com.starts.hencoderview
 
 import android.os.Bundle
-import android.text.*
-import android.text.style.ImageSpan
+import android.text.SpannableString
+import android.text.Spanned
+import android.text.Spanned.SPAN_EXCLUSIVE_EXCLUSIVE
+import android.text.TextPaint
+import android.text.TextUtils
+import android.text.style.DynamicDrawableSpan
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -12,6 +16,7 @@ import com.bytedance.scene.ui.template.AppCompatScene
 import com.starts.hencoderview.SpanUtils.ALIGN_BASELINE
 import com.starts.hencoderview.databinding.SceneEllipsizedTextBinding
 import com.starts.hencoderview.util.QMUIAlignMiddleImageSpan
+import com.starts.hencoderview.util.QMUIAlignMiddleImageSpan.ALIGN_BASELINE
 import com.starts.hencoderview.util.QMUIAlignMiddleImageSpan.ALIGN_MIDDLE
 
 
@@ -22,8 +27,8 @@ import com.starts.hencoderview.util.QMUIAlignMiddleImageSpan.ALIGN_MIDDLE
  *版本号：1.0
 
  */
-class EllipsizedTextScene:AppCompatScene(){
-    lateinit var binding:SceneEllipsizedTextBinding
+class EllipsizedTextScene : AppCompatScene() {
+    lateinit var binding: SceneEllipsizedTextBinding
     override fun onCreateContentView(
         inflater: LayoutInflater,
         container: ViewGroup,
@@ -35,17 +40,45 @@ class EllipsizedTextScene:AppCompatScene(){
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        val drawable = ContextCompat.getDrawable(requireSceneContext() , R.drawable.playing_com_into)!!
-        drawable.setBounds(0, 0 , dp2px(10) , dp2px(10))
+//        val drawable =
+//            ContextCompat.getDrawable(requireSceneContext(), R.drawable.playing_com_into)!!
+//        drawable.setBounds(0, 0, dp2px(10), dp2px(10))
+//
+        val text =
+            "电影主题曲《my heart will go on 》,电影主题曲《myhe"
+//        val span = QMUIAlignMiddleImageSpan(drawable, ALIGN_MIDDLE, -1f)
+//
+//        val spannableString = SpannableString(text)
+//        spannableString.setSpan(
+//            span,
+//            text.length - 2,
+//            text.length,
+//            Spanned.SPAN_EXCLUSIVE_EXCLUSIVE
+//        )
+//        binding.tv1.text = spannableString
 
-        val text = "电影《We are all human》主题曲,电影《We are all human》主题曲,电影《We are all human》主题曲,电影《We are all human》主题曲,电影《We are all human》主题曲,电影《We are all human》主题曲"
-        val span = QMUIAlignMiddleImageSpan(drawable , ALIGN_MIDDLE , -1f)
+        measureTextWith(binding.tv1 ,text )
+    }
 
-        val spannableString = SpannableString(text)
-        spannableString.setSpan(span , text.length -2 , text.length , Spanned.SPAN_EXCLUSIVE_EXCLUSIVE )
-        binding.tv1.text = spannableString
+    private fun measureTextWith(textView: TextView, text: String) {
+        val textWith = resources.displayMetrics.widthPixels
+        val textLength = textWith * 2 - dp2px(10)
+        val paint = textView.paint
+        paint.textSize = textView.textSize
+        val temp = TextUtils.ellipsize(text , paint, textLength * 1f , TextUtils.TruncateAt.END)
+
+        val drawable =
+            ContextCompat.getDrawable(requireSceneContext(), R.drawable.playing_com_into)!!
+        drawable.setBounds(0, 0, dp2px(10), dp2px(10))
+//        val spannableString = SpannableString(temp)
+//        spannableString.setSpan(QMUIAlignMiddleImageSpan(drawable, ALIGN_MIDDLE, -1f) , temp.length-2 ,temp.length -1  , SPAN_EXCLUSIVE_EXCLUSIVE)
+        textView.text = SpanUtils(requireSceneContext())
+            .append(temp)
+            .appendImage(drawable , SpanUtils.ALIGN_BASELINE)
+            .create()
 
     }
 
 
 }
+
