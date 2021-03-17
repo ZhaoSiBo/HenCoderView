a901f3de94d375b314aab2228d1bbede4311c9ae
lorizhao <lorizhao@tencent.com>
2021-03-17 18:46:39 +0800

    1.EllipsizeIconTextView改造升级

--- app/src/main/java/com/starts/hencoderview/EllipsizedTextScene.kt
+++ app/src/main/java/com/starts/hencoderview/EllipsizedTextScene.kt
@@ -21,10 +21,21 @@ import kotlin.math.ceil
 
 /**
 
- *文件描述：.
- *作者：Created by zhaosibo on 2021/2/8.
- *版本号：1.0
-
+ *文件描述：.实现 文字...文字，文字...icon，文字...文字+icon的样式
+ *  当设置了isDisplay = true ，在Ellipsize = end 并且 多行的情况下，图片才会显示，并不会直接追加文本末尾
+ *  keepText不管在当前TextView是在缩略状态，keepText都会被保留
+ *  非缩略状态下，会自动通过SpannableStringBuilder方案实现图文混排
+ *
+ *  作者：Created by Lorizhao on 2021/3/4.
+ *  版本号：1.0
+ *
+ *  使用时，请使用 setTextWithKeepText方法设置 保留图片和 保留文字
+ *  可以单独设置保留文字的 大小，颜色，如果需要修改对齐方式，那就重写在OnDraw（）方法中的绘制方法
+ *
+ *  在onMeasure()方法中判断文字是否会超出，并设置needOverDraw，触发在onDraw()方法中追加绘制图片和文字
+ *  在onMeasure(),onDraw()方法中调用setText()方法后会再次出发onMeasure(),onDraw()，所以要注意结束条件，自测onMeasure()，onDraw()方法的调用次数
+ *  作者：Created by lorizhao on 2021/2/8.
+ *  版本号：1.0
  */
 class EllipsizedTextScene : AppCompatScene() {
     lateinit var binding: SceneEllipsizedTextBinding
