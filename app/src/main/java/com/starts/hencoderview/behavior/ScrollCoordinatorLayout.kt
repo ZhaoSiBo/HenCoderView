0d154bfb7d7d6252830b36bae14ba23f36ca62cb
lorizhao <lorizhao@tencent.com>
2022-04-05 00:02:15 +0800

    1.同步代码

--- app/src/main/java/com/starts/hencoderview/behavior/ScrollCoordinatorLayout.kt
+++ app/src/main/java/com/starts/hencoderview/behavior/ScrollCoordinatorLayout.kt
@@ -2,11 +2,20 @@ package com.starts.hencoderview.behavior
 
 import android.content.Context
 import android.util.AttributeSet
+import android.util.Log
+import android.view.MotionEvent
+import android.view.VelocityTracker
 import android.view.View
+import android.view.ViewConfiguration
+import android.widget.Scroller
 import androidx.coordinatorlayout.widget.CoordinatorLayout
 import com.starts.hencoderview.dp2px
+import kotlin.math.abs
 
 class ScrollCoordinatorLayout : CoordinatorLayout {
+    companion object{
+        const val TAG = "ScrollCoordinatorLayout"
+    }
     constructor(context: Context) : super(context)
     constructor(context: Context, attrs: AttributeSet?) : super(context, attrs)
     constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(
@@ -14,6 +23,12 @@ class ScrollCoordinatorLayout : CoordinatorLayout {
         attrs,
         defStyleAttr
     )
+    // 滚动工具类
+    val scroller = Scroller(context)
+    // 速度计算工具
+    val velocityTracker = VelocityTracker.obtain()
+
+    val viewConfiguration = ViewConfiguration.get(context)
 
     var maxScrollY = 0
 
@@ -32,6 +47,64 @@ class ScrollCoordinatorLayout : CoordinatorLayout {
 
     override fun onLayout(changed: Boolean, l: Int, t: Int, r: Int, b: Int) {
         super.onLayout(changed, l, t, r, b)
+    }
+
+
+    var lastX = 0f
+    var lastY = 0f
+    override fun onInterceptTouchEvent(ev: MotionEvent): Boolean {
+        when(ev.action){
+            MotionEvent.ACTION_DOWN->{
+                lastX = ev.x
+                lastY = ev.y
+                velocityTracker.clear()
+                velocityTracker.addMovement(ev)
+            }
+            MotionEvent.ACTION_MOVE->{
+                Log.d(TAG,"isShouldLindScroll() = ${isShouldLindScroll()} , " +
+                        "${ abs(ev.y - lastY) > abs(ev.x - lastX)},${abs(ev.y - lastY) > viewConfiguration.scaledTouchSlop}")
+                if(isShouldLindScroll() && abs(ev.y - lastY) > abs(ev.x - lastX) && abs(ev.y - lastY) > viewConfiguration.scaledTouchSlop){
+                    return true
+                }else{
+                    lastX = ev.x
+                    lastY = ev.y
+                }
+            }
+        }
+        return super.onInterceptTouchEvent(ev)
+    }
+
+    override fun onTouchEvent(ev: MotionEvent): Boolean {
+        when(ev.action){
+            MotionEvent.ACTION_DOWN -> {
+                // 手指按下时记录 y 轴初始位置
+                lastY = ev.y
+                velocityTracker.clear()
+                velocityTracker.addMovement(ev)
+
+            }
+            MotionEvent.ACTION_MOVE->{
+                val scrollY = ev.y - lastY
+                scrollBy(0 ,scrollY.toInt())
+                lastY = ev.y
+                lastX = ev.x
+                velocityTracker.addMovement(ev)
+            }
+            MotionEvent.ACTION_UP->{
+                velocityTracker.addMovement(ev)
+                velocityTracker.computeCurrentVelocity(1000)
+                val yv = -velocityTracker.yVelocity.toInt()
+                scroller.fling(0, ev.y.toInt(), 0, yv, 0, 0, Int.MIN_VALUE, Int.MAX_VALUE)
+            }
+
+        }
+        return super.onTouchEvent(ev)
+    }
+
+    private fun isShouldLindScroll():Boolean{
+        val topRecyclerView = getChildAt(0)
+        val bottomSheetLayout = getChildAt(1)
+        return  (topRecyclerView.bottom >= bottomSheetLayout.top)
     }
 
 }
--- app/src/main/java/com/starts/hencoderview/behavior/TopContainerBehavior.kt
+++ app/src/main/java/com/starts/hencoderview/behavior/TopContainerBehavior.kt
@@ -2,7 +2,7 @@ package com.starts.hencoderview.behavior
 
 import android.content.Context
 import android.util.AttributeSet
-import android.util.Log
+import android.view.MotionEvent
 import android.view.View
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.coordinatorlayout.widget.CoordinatorLayout
@@ -10,8 +10,6 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.forEachIndexed
 import com.starts.hencoderview.R
 import java.lang.ref.WeakReference
-import kotlin.math.abs
-import kotlin.math.min
 
 /**
 
@@ -25,8 +23,7 @@ class TopContainerBehavior<V : View> : CoordinatorLayout.Behavior<V> {
         val TAG = "TopContainerBehavior"
     }
 
-    var bottomTop = 0
-    var offsetRange = 0
+    var bottomSheetLayoutTop = 0
 
     constructor() : super()
     constructor(context: Context?, attrs: AttributeSet?) : super(context, attrs)
@@ -41,7 +38,7 @@ class TopContainerBehavior<V : View> : CoordinatorLayout.Behavior<V> {
     ): Boolean {
         // Return true if we're nested scrolling vertically, and we either have lift on scroll enabled
         // or we can scroll the children.
-        val result =  viewRef?.get() == directTargetChild &&
+        val result = viewRef?.get() == directTargetChild &&
                 (axes and ViewCompat.SCROLL_AXIS_VERTICAL) != 0
         return result
     }
@@ -69,23 +66,16 @@ class TopContainerBehavior<V : View> : CoordinatorLayout.Behavior<V> {
             consumed
         )
         val overage = overageOffsetValue(dyUnconsumed)
-        if (overage > abs(dyUnconsumed)){
-            Log.d(TAG ,"overage > abs(dyUnconsumed)" )
-            ViewCompat.offsetTopAndBottom(target,-dyUnconsumed)
-        }else if(overage < abs(dyUnconsumed)){
-            Log.d(TAG ,"overage < abs(dyUnconsumed)" )
-            ViewCompat.offsetTopAndBottom(target, -overage)
-        }
+        ViewCompat.offsetTopAndBottom(target, -overage)
     }
 
     override fun onLayoutChild(parent: CoordinatorLayout, child: V, layoutDirection: Int): Boolean {
         val result = super.onLayoutChild(parent, child, layoutDirection)
         viewRef = WeakReference(child)
         bottomSheetBehaviorRef = WeakReference(getBottomSheetBehavior(parent))
-        offsetRange = bottomSheetBehaviorRef?.get()?.peekHeight?:0
         parent.addOnLayoutChangeListener { v, left, top, right, bottom, oldLeft, oldTop, oldRight, oldBottom ->
             val bottom = parent.findViewById<ConstraintLayout>(R.id.layoutBottomSheet)
-            bottomTop = bottom.top
+            bottomSheetLayoutTop = bottom.top
         }
         return result
     }
@@ -93,7 +83,7 @@ class TopContainerBehavior<V : View> : CoordinatorLayout.Behavior<V> {
     private var bottomSheetBehaviorRef: WeakReference<BottomSheetBehavior<*>?>? = null
     private var viewRef: WeakReference<View>? = null
 
-    private fun getBottomSheetBehavior(parent: CoordinatorLayout):BottomSheetBehavior<*>? {
+    private fun getBottomSheetBehavior(parent: CoordinatorLayout): BottomSheetBehavior<*>? {
         parent.forEachIndexed { index, view ->
             val lp = view.layoutParams as CoordinatorLayout.LayoutParams
             val behavior = lp.behavior
@@ -103,21 +93,32 @@ class TopContainerBehavior<V : View> : CoordinatorLayout.Behavior<V> {
         return null
     }
 
+
     /**
-     * @return 剩余偏移量 ,大于0表示还能偏移，小于0 不能偏移
+     * @return 合法的偏移量
      *
      */
-    private fun overageOffsetValue(dyUnconsumed:Int):Int{
-        val topBottom = viewRef?.get()?.bottom?:0
-        val topTop = viewRef?.get()?.top?:0
-        val result = if(dyUnconsumed > 0){
-            topBottom - bottomTop
-        }else{
-            Log.d(TAG ,"topTop = ${topTop}" )
-            0 - topTop
+    private fun overageOffsetValue(dyUnconsumed: Int): Int {
+        val recyclerView = viewRef?.get() ?: return 0
+        // dyUnconsumed > 0 手势向上,recyclerView到底了，无法消耗该值
+        val isFillingToTop = dyUnconsumed > 0
+        if (isFillingToTop) {
+            // 如果recyclerView的底部内容没有完全显示，则为true
+            val isRecyclerNeedOffsetByDy = recyclerView.bottom - dyUnconsumed > bottomSheetLayoutTop
+            return if (isRecyclerNeedOffsetByDy) {
+                dyUnconsumed
+            } else {
+                recyclerView.bottom - bottomSheetLayoutTop
+            }
+        } else {
+            // 如果recyclerView的底部内容没有完全显示，则为true
+            val isRecyclerNeedOffsetByDy = recyclerView.top - dyUnconsumed < 0
+            return if (isRecyclerNeedOffsetByDy) {
+                dyUnconsumed
+            } else {
+                recyclerView.top
+            }
         }
-        Log.d(TAG ,"dyUnconsumed = ${dyUnconsumed} overageOffsetValue = ${result}" )
-        return result
     }
 
 }
