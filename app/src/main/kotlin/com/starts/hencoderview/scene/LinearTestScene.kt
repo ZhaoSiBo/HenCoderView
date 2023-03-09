26f241bdabc6155edd2b2737db70304fac56448d
lorizhao <18772943719@163.com>
2023-03-10 01:48:40 +0800

    1.线性布局的性能测试代码

--- app/src/main/kotlin/com/starts/hencoderview/scene/LinearTestScene.kt
+++ app/src/main/kotlin/com/starts/hencoderview/scene/LinearTestScene.kt
@@ -1,5 +1,6 @@
 package com.starts.hencoderview.scene
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -8,14 +9,19 @@ import com.bytedance.scene.ui.template.AppCompatScene
 import com.starts.hencoderview.databinding.SceneLinearLayoutTestBinding
 
 class LinearTestScene : AppCompatScene() {
-    lateinit var binding:SceneLinearLayoutTestBinding
+    lateinit var binding: SceneLinearLayoutTestBinding
     override fun onCreateContentView(p0: LayoutInflater, p1: ViewGroup, p2: Bundle?): View {
         binding = SceneLinearLayoutTestBinding.inflate(p0)
         return binding.root
     }
 
+    @SuppressLint("SetTextI18n")
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         setTitle("LinearTestScene")
+        binding.tvShort.postDelayed({
+            binding.tvShort.text = "现在我变成了长文字，现在我变成了长文字，现在我变成了长文字，现在我变成了长文字，" +
+                    "现在我变成了长文字现在我变成了长文字，现在我变成了长文字，现在我变成了长文字，现在我变成了长文字"
+        }, 4000)
     }
 }
--- app/src/main/kotlin/com/starts/hencoderview/view/TestTextView.kt
+++ app/src/main/kotlin/com/starts/hencoderview/view/TestTextView.kt
@@ -5,34 +5,21 @@ import android.graphics.Canvas
 import android.graphics.Color
 import android.graphics.Paint
 import android.util.AttributeSet
-import android.widget.LinearLayout
 import timber.log.Timber
 
-class TestLinearLayout :LinearLayout {
-    init {
-        setWillNotDraw(false)
-
-    }
+class TestTextView :androidx.appcompat.widget.AppCompatTextView {
     val paint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
-        color = Color.BLACK
+        color = Color.WHITE
         textSize = 32f
     }
     var measureCount  = 0
-    constructor(context: Context?) : super(context)
-    constructor(context: Context?, attrs: AttributeSet?) : super(context, attrs)
-    constructor(context: Context?, attrs: AttributeSet?, defStyleAttr: Int) : super(
+    constructor(context: Context) : super(context)
+    constructor(context: Context, attrs: AttributeSet?) : super(context, attrs)
+    constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(
         context,
         attrs,
         defStyleAttr
     )
-
-    constructor(
-        context: Context?,
-        attrs: AttributeSet?,
-        defStyleAttr: Int,
-        defStyleRes: Int
-    ) : super(context, attrs, defStyleAttr, defStyleRes)
-
 
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
         super.onMeasure(widthMeasureSpec, heightMeasureSpec)
@@ -41,7 +28,7 @@ class TestLinearLayout :LinearLayout {
 
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
-        canvas.drawText("measureCount = $measureCount" , 40f,80f,paint)
+        canvas.drawText("measureCount = $measureCount" , 40f,40f,paint)
     }
 
 }
